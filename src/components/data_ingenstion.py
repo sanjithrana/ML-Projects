5e076209378fc34aff29d8bd15ce434f012f32ee
sanjithgit <141348079+sanjithrana@users.noreply.github.com.>
2026-01-19 20:38:13 +0530

    data_ingenstion

---
+++ src/components/data_ingenstion.py
@@ -0,0 +1,48 @@
+# this the a place where data is present
+from dataclasses import dataclass
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd  
+import os
+import sys
+from sklearn.model_selection import train_test_split
+
+@dataclass #because inside a class to define the class variable we use __init__
+           #but with the help of this we can directly define the class variable
+class DataIngenstionConfig:
+    train_data_path: str = os.path.join("artifacts","train.csv")
+    test_data_path: str = os.path.join("artifacts","test.csv")
+    raw_data_path: str = os.path.join("artifacts","data.csv")
+
+#here we need the functions so we are not using the decarator
+
+class DataIngenstion:
+    def __init__(self):
+        self.ingenstion_config = DataIngenstionConfig()
+    def initiate_ingenstion_data(self):#we our data is stored in any database we have to call
+        logging.info("Enter the data ingestion method or components")
+        try:
+            df= pd.read_csv("src//notebook//data//stud.csv")
+            logging.info('Read the Dataest as DataFrame')
+
+            os.makedirs(os.path.dirname(self.ingenstion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingenstion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initited")
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingenstion_config.train_data_path,index = False,header = True)
+            test_set.to_csv(self.ingenstion_config.train_data_path,index = False,header = True)
+
+            logging.info("ingenstion of the data is happen")
+
+            return( self.ingenstion_config.train_data_path,
+                     self.ingenstion_config.test_data_path
+                     )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__ == "__main__":#initiate
+    obj = DataIngenstion()
+    obj.initiate_ingenstion_data()
